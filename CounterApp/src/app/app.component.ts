17e1adb8106d9b4108fdc70794254098cc9709fb
fatima.oujeddi <fa.oujeddi@gmail.com>
2017-09-20 14:10:12 -0500

    s

---
+++ CounterApp/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class AppComponent {
+  title = 'Counter App';
+  public componentCounterValue:number;
+  
+    constructor() { 
+      this.componentCounterValue=0;
+    }
+
+    setCounter(val) {
+      if(!isNaN(val))
+        this.componentCounterValue = val;
+    }
+
+    updateCounterFromChild(val) {
+      this.componentCounterValue = val;
+    }
+}
---
+++ CounterApp/src/app/counter.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  inputs: ['counterValue'],
+  styleUrls: ['./app.component.css'],
+  template: `  
+    <h1>Counter Component</h1>
+    <button (click)="increment()">+</button>
+    {{counterValue}}
+    <button (click)="decrement()">-</button>
+    <br><br><button (click)="reset()">Reset</button>
+  `
+})
+export class CounterComponent implements OnInit {
+  title = 'Counter App';
+  private counterValue;
+  
+  constructor() { 
+    this.counterValue = 0;
+  }
+
+  @Output()
+  counterChange = new EventEmitter();
+
+  increment(){
+    this.counterValue++;
+    this.counterChange.emit(this.counterValue);
+  }
+
+  decrement(){
+    this.counterValue--;
+    this.counterChange.emit(this.counterValue);
+    return false;
+  }
+  
+  reset(){
+    this.counterValue = 0;
+  }
+
+  ngOnInit() {
+  }
+}
